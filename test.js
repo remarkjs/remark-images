2a07c07105c485355f0cca1a42270a3ef4d53273
Titus Wormer <tituswormer@gmail.com>
2023-09-20 16:06:28 +0200

    Refactor to use `node:test`

--- test.js
+++ test.js
@@ -1,151 +1,190 @@
-import test from 'tape'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import {remark} from 'remark'
 import remarkImages, {defaultImageExtensions} from './index.js'
 
-test('remarkImages', (t) => {
-  t.equal(
-    remark().use(remarkImages).processSync('<https://example.com>').toString(),
-    '<https://example.com>\n',
-    'should not support non-image URLs'
-  )
+test('remarkImages', async function (t) {
+  await t.test('should not support non-image URLs', async function () {
+    assert.equal(
+      remark()
+        .use(remarkImages)
+        .processSync('<https://example.com>')
+        .toString(),
+      '<https://example.com>\n'
+    )
+  })
 
-  t.equal(
-    remark()
-      .use(remarkImages)
-      .processSync('https://example.com/example.jpg')
-      .toString(),
-    '[![](https://example.com/example.jpg)](https://example.com/example.jpg)\n',
-    'should support absolute URLs to images'
-  )
+  await t.test('should support absolute URLs to images', async function () {
+    assert.equal(
+      remark()
+        .use(remarkImages)
+        .processSync('https://example.com/example.jpg')
+        .toString(),
+      '[![](https://example.com/example.jpg)](https://example.com/example.jpg)\n'
+    )
+  })
 
-  t.equal(
-    remark().use(remarkImages).processSync('/example.jpg').toString(),
-    '[![](/example.jpg)](/example.jpg)\n',
-    'should support absolute paths to images'
-  )
+  await t.test('should support absolute paths to images', async function () {
+    assert.equal(
+      remark().use(remarkImages).processSync('/example.jpg').toString(),
+      '[![](/example.jpg)](/example.jpg)\n'
+    )
+  })
 
-  t.equal(
-    remark().use(remarkImages).processSync('./example.jpg').toString(),
-    '[![](./example.jpg)](./example.jpg)\n',
-    'should support relative paths to images'
-  )
+  await t.test('should support relative paths to images', async function () {
+    assert.equal(
+      remark().use(remarkImages).processSync('./example.jpg').toString(),
+      '[![](./example.jpg)](./example.jpg)\n'
+    )
+  })
 
-  t.equal(
-    remark().use(remarkImages).processSync('../example.jpg').toString(),
-    '[![](../example.jpg)](../example.jpg)\n',
-    'should support very relative paths to images'
-  )
-
-  t.equal(
-    remark()
-      .use(remarkImages)
-      .processSync('<https://example.com/example.jpg>')
-      .toString(),
-    '[![](https://example.com/example.jpg)](https://example.com/example.jpg)\n',
-    'should support autolinks'
-  )
-
-  t.equal(
-    remark()
-      .use(remarkImages)
-      .processSync(
-        '[https://example.com/alpha.jpg](https://example.com/bravo.jpg)'
+  await t.test(
+    'should support very relative paths to images',
+    async function () {
+      assert.equal(
+        remark().use(remarkImages).processSync('../example.jpg').toString(),
+        '[![](../example.jpg)](../example.jpg)\n'
       )
-      .toString(),
-    '[![](https://example.com/alpha.jpg)](https://example.com/bravo.jpg)\n',
-    'should support links'
+    }
   )
 
-  t.equal(
-    remark()
-      .use(remarkImages)
-      .processSync(
-        '[https://example.com/alpha.jpg][1]\n\n[1]: https://example.com/bravo.jpg'
+  await t.test('should support autolinks', async function () {
+    assert.equal(
+      remark()
+        .use(remarkImages)
+        .processSync('<https://example.com/example.jpg>')
+        .toString(),
+      '[![](https://example.com/example.jpg)](https://example.com/example.jpg)\n'
+    )
+  })
+
+  await t.test('should support links', async function () {
+    assert.equal(
+      remark()
+        .use(remarkImages)
+        .processSync(
+          '[https://example.com/alpha.jpg](https://example.com/bravo.jpg)'
+        )
+        .toString(),
+      '[![](https://example.com/alpha.jpg)](https://example.com/bravo.jpg)\n'
+    )
+  })
+
+  await t.test('should support link references', async function () {
+    assert.equal(
+      remark()
+        .use(remarkImages)
+        .processSync(
+          '[https://example.com/alpha.jpg][1]\n\n[1]: https://example.com/bravo.jpg'
+        )
+        .toString(),
+      '[![](https://example.com/alpha.jpg)][1]\n\n[1]: https://example.com/bravo.jpg\n'
+    )
+  })
+
+  await t.test(
+    'should support image URLs inside other stuff',
+    async function () {
+      assert.equal(
+        remark()
+          .use(remarkImages)
+          .processSync('**https://example.com/alpha.jpg**')
+          .toString(),
+        '**[![](https://example.com/alpha.jpg)](https://example.com/alpha.jpg)**\n'
       )
-      .toString(),
-    '[![](https://example.com/alpha.jpg)][1]\n\n[1]: https://example.com/bravo.jpg\n',
-    'should support link references'
+    }
   )
 
-  t.equal(
-    remark()
-      .use(remarkImages)
-      .processSync('**https://example.com/alpha.jpg**')
-      .toString(),
-    '**[![](https://example.com/alpha.jpg)](https://example.com/alpha.jpg)**\n',
-    'should support image URLs inside other stuff'
-  )
-
-  t.equal(
-    remark()
-      .use(remarkImages)
-      .processSync(
-        '[**https://example.com/alpha.jpg**](https://example.com/bravo.jpg)'
+  await t.test(
+    'should support image URLs inside other stuff in links',
+    async function () {
+      assert.equal(
+        remark()
+          .use(remarkImages)
+          .processSync(
+            '[**https://example.com/alpha.jpg**](https://example.com/bravo.jpg)'
+          )
+          .toString(),
+        '[**![](https://example.com/alpha.jpg)**](https://example.com/bravo.jpg)\n'
       )
-      .toString(),
-    '[**![](https://example.com/alpha.jpg)**](https://example.com/bravo.jpg)\n',
-    'should support image URLs inside other stuff in links'
+    }
   )
 
-  t.equal(
-    remark().use(remarkImages).processSync('/example.png').toString(),
-    '[![](/example.png)](/example.png)\n',
-    'should support png extension as image'
+  await t.test('should support png extension as image', async function () {
+    assert.equal(
+      remark().use(remarkImages).processSync('/example.png').toString(),
+      '[![](/example.png)](/example.png)\n'
+    )
+  })
+
+  await t.test('should support svg extension as image', async function () {
+    assert.equal(
+      remark().use(remarkImages).processSync('/example.svg').toString(),
+      '[![](/example.svg)](/example.svg)\n'
+    )
+  })
+
+  await t.test('should support jpeg extension as image', async function () {
+    assert.equal(
+      remark().use(remarkImages).processSync('/example.jpeg').toString(),
+      '[![](/example.jpeg)](/example.jpeg)\n'
+    )
+  })
+
+  await t.test('should support gif extension as image', async function () {
+    assert.equal(
+      remark().use(remarkImages).processSync('/example.gif').toString(),
+      '[![](/example.gif)](/example.gif)\n'
+    )
+  })
+
+  await t.test('should support webp extension as image', async function () {
+    assert.equal(
+      remark().use(remarkImages).processSync('/example.webp').toString(),
+      '[![](/example.webp)](/example.webp)\n'
+    )
+  })
+
+  await t.test('should support avif extension as image', async function () {
+    assert.equal(
+      remark().use(remarkImages).processSync('/example.avif').toString(),
+      '[![](/example.avif)](/example.avif)\n'
+    )
+  })
+
+  await t.test(
+    'should support custom extension as image through options',
+    async function () {
+      assert.equal(
+        remark()
+          .use(remarkImages, {imageExtensions: ['custom']})
+          .processSync('/example.custom')
+          .toString(),
+        '[![](/example.custom)](/example.custom)\n'
+      )
+    }
   )
 
-  t.equal(
-    remark().use(remarkImages).processSync('/example.svg').toString(),
-    '[![](/example.svg)](/example.svg)\n',
-    'should support svg extension as image'
+  await t.test(
+    'should support removing a default extension',
+    async function () {
+      assert.equal(
+        remark()
+          .use(remarkImages, {imageExtensions: ['custom']})
+          .processSync('/example.jpg')
+          .toString(),
+        '/example.jpg\n'
+      )
+    }
   )
 
-  t.equal(
-    remark().use(remarkImages).processSync('/example.jpeg').toString(),
-    '[![](/example.jpeg)](/example.jpeg)\n',
-    'should support jpeg extension as image'
+  await t.test(
+    'should export default image extensions for reuse',
+    async function () {
+      assert.ok(
+        Array.isArray(defaultImageExtensions) &&
+          defaultImageExtensions.every((ext) => typeof ext === 'string')
+      )
+    }
   )
-
-  t.equal(
-    remark().use(remarkImages).processSync('/example.gif').toString(),
-    '[![](/example.gif)](/example.gif)\n',
-    'should support gif extension as image'
-  )
-
-  t.equal(
-    remark().use(remarkImages).processSync('/example.webp').toString(),
-    '[![](/example.webp)](/example.webp)\n',
-    'should support webp extension as image'
-  )
-
-  t.equal(
-    remark().use(remarkImages).processSync('/example.avif').toString(),
-    '[![](/example.avif)](/example.avif)\n',
-    'should support avif extension as image'
-  )
-
-  t.equal(
-    remark()
-      .use(remarkImages, {imageExtensions: ['custom']})
-      .processSync('/example.custom')
-      .toString(),
-    '[![](/example.custom)](/example.custom)\n',
-    'should support custom extension as image through options'
-  )
-
-  t.equal(
-    remark()
-      .use(remarkImages, {imageExtensions: ['custom']})
-      .processSync('/example.jpg')
-      .toString(),
-    '/example.jpg\n',
-    'should support removing a default extension'
-  )
-
-  t.true(
-    Array.isArray(defaultImageExtensions) &&
-      defaultImageExtensions.every((ext) => typeof ext === 'string'),
-    'should export default image extensions for reuse'
-  )
-
-  t.end()
 })
